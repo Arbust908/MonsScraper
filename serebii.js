2ea31c3b48ae5acbe73c3621e377fcbed3187316
Arbust0 <Arbust908@gmail.com>
2019-11-29 22:21:44 -0300

    Nuevas cosas

--- serebii.js
+++ serebii.js
@@ -32,53 +32,89 @@ const createFolder = (name) => {
                 let pkmn = {};
                 for (let i = 0; i < celda.length; i++) {
                     const item = celda[i];
-                    switch (i) {
-                        case 0:
-                            pkmn.no = item.innerText;
-                            break;
+                     switch (i) {
+                         case 0:
+                             number = item.innerText;
+                             if (number) {
+                                 pkmn.no = item.innerText.split('#')[1];
+                             }
+                             break;
                         case 1:
                             var images = '';
-                            images = item.innerHTML.split('src=')[1];
-                            images = images.split('"')[1];
-                            pkmn.img = images;
+                            images = item.innerHTML
+                            pkmn.a = images;
+                            if (images) {
+                                pkmn.a = images.split('a href=')[1].split('"')[1];
+                                images = images.split('src=')[1];
+                                images = images.split('"')[1];
+                                pkmn.img = images;
+                            }
                             break;
-                        case 3:
-                            pkmn.name = item.innerText.split('\n')[0];
-                            pkmn.trueName = item.innerText.split('\n')[1];
-                        case 4:
-                            pkmn.abilities = item.innerText.split('\n');
+                         case 3:
+                             pkmn.name = item.innerText.split('\n')[0];
+                             pkmn.trueName = item.innerText.split('\n')[1];
+                         case 4:
+                             pkmn.abilities = item.innerText.split('\n');
                         case 5:
                             var types = '';
-                            types = item.innerHTML.split('src=')[1];
+                            types = item.innerHTML;
                             if(types) {
+                                types = types.split('src=');
                                 pkmn.noty = types.length;
-                                types = types.split('"')[1];
+                                pkmn.types = [];
+                                if (pkmn.noty >= 2) {
+                                    for (let x = 1; x < types.length; x++) {
+                                        const tipo = types[x];
+                                        pkmn.types.push( tipo.split('"')[1] );
+                                    }
+                                }
+                                //types = types[1];
+                                //types = types.split('"')[1];
                             }
-                            pkmn.type = types;
-                        case 6:
-                            pkmn.hp = item.innerText;
-                        case 7:
-                            pkmn.attack = item.innerText;
-                        case 8:
-                            pkmn.defence = item.innerText;
-                        case 9:
-                            pkmn.special_attack = item.innerText;
-                        case 10:
-                            pkmn.special_defence = item.innerText;
-                        case 11:
-                            pkmn.speed = item.innerText;
-                        default:
+                            //pkmn.type = types;
+                         case 6:
+                             pkmn.hp = item.innerText;
+                         case 7:
+                             pkmn.attack = item.innerText;
+                         case 8:
+                             pkmn.defence = item.innerText;
+                         case 9:
+                             pkmn.special_attack = item.innerText;
+                         case 10:
+                             pkmn.special_defence = item.innerText;
+                         case 11:
+                             pkmn.speed = item.innerText;
+                         default:
                             break;
-                    }
+                     }
                     //pkmn[i] = item.innerText;
                 }
-                pkmns.push(pkmn);
+                if(pkmn.noty) {
+                    pkmns.push(pkmn);
+                }
             }
         }
         return pkmns;
     });
 
     console.log('pokemon', pkmns);
+
+    console.log('---< o >---');
+
+    const pkmnsJson = JSON.stringify(pkmns);
+    fs.writeFileSync('./serebii/pokes.json', pkmnsJson);
+    console.log('Escribimos');
+    pkmns.forEach( pkmn  => {
+        let dirName = slugify(pkmn.no);
+        createFolder('./serebii/'+ dirName );
+        fs.writeFile(
+            './serebii/'+ dirName +'/'+ dirName +'.json',
+            JSON.stringify(pkmn),
+            (err) => {
+                if (err) console.log('Error writing file:', err)
+            }
+        )
+    });
 
     console.log('---< o >---');
     console.log('---< o >---');
